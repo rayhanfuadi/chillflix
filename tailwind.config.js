764e0528a9f6f4b49a15940ae257ead8c9c118db
rayhanfuadi <oke.rayhan1@gmail.com>
2024-08-15 23:25:30 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    extend: {
+       backgroundImage: {
+        'bg-pattern': "url('/img/masuk.jpg')",
+       }
+    },
+  },
+  plugins: [],
+}
+
