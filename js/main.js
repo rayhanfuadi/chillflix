288f15c3bbe320c4dbf093d9080b0afa012e7ec6
rayhanfuadi <oke.rayhan1@gmail.com>
2024-08-20 02:48:40 +0700

    fix: fixed feature home page!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Swiper JS
 const swiperEl = document.querySelector('swiper-container')
     Object.assign(swiperEl, {
       slidesPerView: 1,
@@ -11,7 +12,7 @@ const swiperEl = document.querySelector('swiper-container')
           spaceBetween: 20,
         },
         768: {
-          slidesPerView: 4,
+          slidesPerView: 3,
           spaceBetween: 24,
         },
         1024: {
@@ -21,3 +22,28 @@ const swiperEl = document.querySelector('swiper-container')
       },
     });
     swiperEl.initialize();
+
+    //pagedone JS untuk Top Rating
+const swiper = new Swiper(".multiple-slide-carousel", {
+         loop: true,
+         slidesPerView: 3,
+         spaceBetween: 16,
+         navigation: {
+            prevEl: ".multiple-slide-carousel .swiperPrev",
+            nextEl: ".multiple-slide-carousel .swiperNext",
+         },
+         breakpoints: {
+          1024: {
+              slidesPerView: 5,
+              spaceBetween: 24
+          },
+          768: {
+              slidesPerView: 3,
+              spaceBetween: 24
+          },
+          640: {
+              slidesPerView: 2,
+              spaceBetween: 24
+          }
+        }
+       });
